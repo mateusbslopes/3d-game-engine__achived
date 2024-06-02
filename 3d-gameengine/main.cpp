a9b7e09f6f68437b12c8090bf0d0e83e22e45dbb
Mateus Lopes <mateusbslopes@gmail.com>
2024-06-02 09:12:10 -0300

    Initial Commit

---
+++ 3d-gameengine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  3d-gameengine
+//
+//  Created by Mateus on 02/06/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
