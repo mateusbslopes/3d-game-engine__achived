3ed4e7f8751d49fac6bd7dc4dba3f3ba8ee35610
Mateus Lopes <mateusbslopes@gmail.com>
2024-06-02 11:51:10 -0300

    feature: create vulkan instance

--- 3d-game-engine/main.cpp
+++ 3d-game-engine/main.cpp
@@ -6,6 +6,8 @@
 #include <stdexcept>
 #include <cstdlib>
 
+#include <vector>
+
 class HelloTriangleApplication {
 public:
     void run() {
@@ -16,8 +18,10 @@ public:
     }
 
 private:
-    const int WIDTH = 800, HEIGHT = 600;
+    // uint32_t for portability https://stackoverflow.com/questions/20077313/uint32-t-vs-int-as-a-convention-for-everyday-programming
+    const uint32_t WIDTH = 800, HEIGHT = 600;
     GLFWwindow* window;
+    VkInstance instance;
     
     void initWindow() {
         glfwInit();
@@ -30,8 +34,51 @@ private:
         window = glfwCreateWindow(WIDTH, HEIGHT, "3d engine", nullptr, nullptr);
     }
     
-    void initVulkan() {
+    void createInstance(){
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "3d Engine";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        
+        uint32_t glfwExtensionCount = 0;
+        // Poiter to pointer "**" means
+        const char** glfwExtensions;
+        
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+        
+        // start Apple compatibility
+        std::vector<const char*> requiredExtensions;
 
+        for(uint32_t i = 0; i < glfwExtensionCount; i++) {
+            requiredExtensions.emplace_back(glfwExtensions[i]);
+        }
+
+        requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+        createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
+        createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+        // end Apple compatibility
+        
+        
+        if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS){
+            throw std::runtime_error("Failed to create instance!");
+        }
+    }
+    
+    void initVulkan() {
+        createInstance();
     }
 
     void mainLoop() {
